7c6a164bc7e79e4cdfa19d49ec52a3319ba59b1a
Matthew <bushmat4@gmail.com>
2013-08-29 09:14:04 -0500

    Fixed script pathways

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+        <script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>/js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
 
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
+        <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/plugins.js"></script>
+        <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
 
         <script>
             var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
