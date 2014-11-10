db01d8b4cb886c39fb46c5910369faa5835e9887
Rodney Dennis <rdnydnns@gmail.com>
2014-11-10 16:05:27 -0600

    update jshint and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ uglify       = require('gulp-uglify'),
 rename       = require('gulp-rename'),
 concat       = require('gulp-concat'),
 notify       = require('gulp-notify'),
+imagemin     = require('gulp-imagemin'),
 cache        = require('gulp-cache'),
 livereload   = require('gulp-livereload'),
 plumber      = require('gulp-plumber'),
@@ -20,8 +21,8 @@ gulp.task('styles', function() {
       style: 'compressed'
     }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest(''))
-    .pipe(notify({message: 'Finished styles!'}));
+    .pipe(gulp.dest('.'))
+    .pipe(notify({message: 'Completed Styles'}));
 });
 
 // scripts
@@ -32,8 +33,8 @@ gulp.task('scripts', ['modernizr'], function() {
     .pipe(jshint.reporter('default'))
     .pipe(concat('all.js'))
     .pipe(uglify())
-    .pipe(gulp.dest(''))
-    .pipe(notify({message: 'Finished scripts!'}));
+    .pipe(gulp.dest('.'))
+    .pipe(notify({message: 'Completed Scripts'}));
 });
 
 // modernizr
@@ -44,22 +45,36 @@ gulp.task('modernizr', function() {
     .pipe(gulp.dest('assets/scripts/vendor/'))
 });
 
+// images
+gulp.task('images', function() {
+  return gulp.src('assets/images/**/*')
+    .pipe(plumber())
+    .pipe(cache(imagemin({
+      optimizationLevel:5,
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(notify({message: 'Completed Images'}))
+    .pipe(gulp.dest('assets/images/'))
+});
+
 // clean
 gulp.task('clean', function(cb) {
-  del(['/style.css', '/all.js'], cb)
+  del(['style.css', 'all.js'], cb)
 });
 
 // watch
 gulp.task('watch', function() {
-  gulp.watch('./assets/styles/**/*.scss', ['styles']);
-  gulp.watch('./assets/scripts/**/*.js', ['scripts']);
+  gulp.watch('assets/styles/**/*.scss', ['styles']);
+  gulp.watch('assets/scripts/**/*.js', ['scripts']);
+  gulp.watch('assets/images/**', ['images']);
   // Create LiveReload server
   livereload.listen();
   // Watch for assets and templates, reload on change
-  gulp.watch(['**/*.php', 'assets/**/*']).on('change', livereload.changed);
+  gulp.watch(['**/*.php', 'assets/**']).on('change', livereload.changed);
 });
 
 // default
-gulp.task('default', ['clean', 'watch'], function() {
-  gulp.start(['styles', 'scripts']);
+gulp.task('default', ['clean', 'styles', 'scripts', 'images'], function() {
+  gulp.start('watch');
 });
